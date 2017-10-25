a28e91ca83e26b3470297dac3fc04c552ec475a6
stanhopec <cstanhope2@nait.ca>
2017-10-25 13:21:45 -0600

    End Wednesday session with questions for Thursday

---
+++ Cool Cat Records Examples/2017-10-25_joinsExamples.sql
@@ -0,0 +1,37 @@
+-- EXAMPLE 1 --
+-- select AlbumID, AlbumName, ArtistName, and Genre ID 
+select AlbumID, AlbumName, ArtistName, GenreID from Album
+inner join Artist on Album.ArtistID = Artist.ArtistID
+
+-- EXAMPLE 2 --
+-- select Employee's full name, how many repeipts they are attached to 
+select firstname + ' ' + lastname 'Employee Name', count(receiptID) from Employee
+inner join Receipt on Receipt.EmployeeID = Employee.EmployeeID
+group by firstname, lastname
+
+-- Now, if I want to check that this data is correct, I'm going to modify my answer up top:
+select firstname + ' ' + lastname 'Employee Name', receiptID from Employee
+inner join Receipt on Receipt.EmployeeID = Employee.EmployeeID
+order by employee.EmployeeID
+
+-- now I can see how many receipts "Stan McMann" is attached to and double check that it's the same as my answer above!
+
+-- EXAMPLE 3 --
+-- take example 2 and modify it so that you show the total amount of money the employee has sold (hint: use subtotal)
+
+-- EXAMPLE 4 --
+-- show all employees whose total amount of money sold is greater than 200$.
+
+-- EXAMPLE 5 --
+-- Show me the biggest purchase a customer has made and show me their full name and phone number
+
+-- EXAMPLE 6 --
+-- select AlbumID, AlbumName, ArtistName, and Genre Description
+-- HINT: there are three tables here!
+
+-- EXAMPLE 7 --
+-- show me all the customers who had purchases greater than 50$ and show me what kind of payment they used (PaymentType description)
+
+-- EXAMPLE 8 --
+-- show the AlbumName, ArtistName, the current price of the album, and how many times it has been sold
+-- HINT: you need three tables here
