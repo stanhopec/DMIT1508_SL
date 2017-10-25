7a98b5cdee6028c09a66bc47bab23715b9a9c79f
stanhopec <cstanhope2@nait.ca>
2017-10-25 12:58:33 -0600

    End Wednesday Session (incomplete)

---
+++ Cool Cat Records Examples/2017-10-25_selectExamples.sql
@@ -0,0 +1,29 @@
+-- select AlbumID, AlbumName, ArtistName, and Genre ID 
+select AlbumID, AlbumName, ArtistName, GenreID from Album
+inner join Artist on Album.ArtistID = Artist.ArtistID
+
+-- select Employee's full name, how many repeipts they are attached to 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- select AlbumID, AlbumName, ArtistName, and Genre Description
+-- HINT: there are three tables here!
+
+-- select all Albums that have never been sold
