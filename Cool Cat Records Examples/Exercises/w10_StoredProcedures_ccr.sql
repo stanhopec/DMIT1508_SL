3a1d70e0b9f2031cbe465da246dc6056398c7ce5
stanhopec <cstanhope2@nait.ca>
2017-11-07 09:30:46 -0700

    Added a Stored Procedures Exercise

---
+++ Cool Cat Records Examples/Exercises/w10_StoredProcedures_ccr.sql
@@ -0,0 +1,23 @@
+-- EXAMPLE 1 ----------------------------------------------------------------------------------------------------------------------------------------------
+-- create a procedure that shows AlbumName, ArtistName, CurrentPrice, and QOH. Just one big dump of data!
+
+-- EXAMPLE 2 ----------------------------------------------------------------------------------------------------------------------------------------------
+-- change the above procedure to only show the albums sold in 2011. Call it "AllAlbums2011"
+
+
+-- EXAMPLE 3 ----------------------------------------------------------------------------------------------------------------------------------------------
+-- create a procedure called "SearchAlbumByKeyword" that takes a string as a parameter and returns all Album names that contain that string.
+-- show the album name, artist name, and albumID.
+
+
+-- EXAMPLE 4 ----------------------------------------------------------------------------------------------------------------------------------------------
+-- modify the above procedure to search the passed keyword in exists in either Album name OR Artist name. Display the same values as above. 
+-- Call it "SearchAlbumOrArtistByKeyword"
+
+
+-- EXAMPLE 5 ----------------------------------------------------------------------------------------------------------------------------------------------
+-- change the Example 2 procedure to take a year as a parameter and return a list of all albums sold in that year. Call it "AlbumsByYearSold"
+
+-- this is a big problem, let's take a smaller bite from it and just get the select statement working with a hardcoded year, then move on to the procedure. 
+
+-- Please let Claire know when you think you've got this select statement done before moving on :)
