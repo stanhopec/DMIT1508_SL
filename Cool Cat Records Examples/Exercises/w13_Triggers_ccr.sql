55217c33aa6538b5f42de1e845902cb86de06fbe
stanhopec <cstanhope2@nait.ca>
2017-11-28 09:32:28 -0700

    Updated ERD, Triggers Exercises, and other stuffs

--- Cool Cat Records Examples/Exercises/w13_Triggers_ccr.sql
+++ Cool Cat Records Examples/Exercises/w13_Triggers_ccr.sql
@@ -12,5 +12,7 @@
 -- EXAMPLE 2 -- "tr_ReceiptAlbum_Insert" -----------------------------------------------------------------------------------------------------------------------------------------------
 -- Create a trigger that updates the quantity on hand of an album when a new Receipt Album is created
 
--- EXAMPLE 3 -- "tr_PurchaseOrder_UpdateInsert" ----------------------------------------------------------------------------------------------------------------------------------------
--- when the status of a purchase order is changed to "received", update the quantity on hands for all of the albums in that order to reflect the new shipment.
+-- EXAMPLE 3 -- "tr_LimitArtist_UpdateInsert" ----------------------------------------------------------------------------------------------------------------------------------------
+-- For some reason, my business doesn't want one artist to have more than four albums. Don't ask why, it's a weird business. 
+-- NOTE: if my business has been going on for a while, I might already have an artist with 5 albums. I don't want this to affect that, 
+-- I just want it to prevent this in the future! So you'll have to NARROW YOUR SEARCH AND LOOK AT RECORDS THAT YOU JUST INSERTED. HINT HINT.
--- Cool Cat Records Examples/Triggers_Demo.sql
+++ Cool Cat Records Examples/Triggers_Demo.sql
@@ -27,6 +27,32 @@ set FirstName = 'Robert'
 	where CustomerID = 102
 
 
+go
+create trigger THE_UPDATE_KEYWORD
+on Customer
+for insert as
+-- for insert, update as
+-- for insert, update, delete as
+if @@ROWCOUNT > 0 and update(Email)
+	begin
+	-- these two tables will only print out if the above if statments are true!
+	select * from inserted
+	select * from deleted
+
+	-- make sure we don't actually commit our changes!
+	rollback transaction
+	end
+return
+go
+
+
+insert into Customer(FirstName, LastName, Phone, Address, City, Province, PostalCode, Country)
+	values ('Carl', 'Sternhop', '1-866-265-2651', '123 4th Street', 'Victoria', 'BC', 'V4T5H2', 'Canada')
+
+delete Customer where CustomerID = 110
+
+select * from Customer
+
 
 /*************************************************************************************************************
  **********************************  CLAIRE's TRIGGER CHEAT SHEET  *******************************************
